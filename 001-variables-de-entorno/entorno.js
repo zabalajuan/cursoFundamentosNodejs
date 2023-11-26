9ed21c50c08fdb6bb5213d8ce3970ec19a4bdc96
Juan Zabala <zabala1juan@gmail.com>
2023-11-25 21:57:28 -0500

    Herramientas apoyo Nodemon and PM2

--- 001-variables-de-entorno/entorno.js
+++ 001-variables-de-entorno/entorno.js
@@ -4,6 +4,7 @@ let web = process.env.WEB || 'sin web';
 ;
 console.log('Hola '+ nombre);
 console.log('Mi web es '+ web);
+console.log('probando Nodemon');
 //para ejecutarlo, asignando la variable nombre, le damos el comando a la terminal:
 // NOMBRE=Carlos node 001-variables-de-entorno/entorno.js
 // NOMBRE=Carlos WEB=juanzabala.tech node 001-variables-de-entorno/entorno.js
