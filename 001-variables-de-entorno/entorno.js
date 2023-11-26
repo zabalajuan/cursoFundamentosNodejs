1da212119da6bce5af4a246166216a3691139a1c
Juan Zabala <zabala1juan@gmail.com>
2023-11-25 21:36:11 -0500

    001-variables de entorno

---
+++ 001-variables-de-entorno/entorno.js
@@ -0,0 +1,9 @@
+// Creamos una variable de entorno
+let nombre = process.env.NOMBRE || 'sin nombre';
+let web = process.env.WEB || 'sin web';
+;
+console.log('Hola '+ nombre);
+console.log('Mi web es '+ web);
+//para ejecutarlo, asignando la variable nombre, le damos el comando a la terminal:
+// NOMBRE=Carlos node 001-variables-de-entorno/entorno.js
+// NOMBRE=Carlos WEB=juanzabala.tech node 001-variables-de-entorno/entorno.js
